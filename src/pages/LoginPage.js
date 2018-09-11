ae7bb71c98379663e9f1a28ca01c60269d9bda30
Henri Koskinen <hkoskinen@protonmail.com>
2018-09-11 23:24:16 +0300

    Add login page and login component

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,13 +1,57 @@
 import React from 'react';
+import axios from 'axios';
+
+
+class Login extends React.Component {
+
+  state = {
+    username: '',
+    password: ''
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    axios.post('http://localhost:64000/login', {
+      username: this.state.username,
+      password: this.state.password
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer fasfasfasf`
+      }
+    })
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit}>
+        <p><label>username:
+          <input type="text" name="username"
+            value={this.state.username}
+            onChange={(e) => this.setState({username: e.target.value})} />
+        </label></p>
+        <p><label>password:
+          <input type="password" name="password"
+            value={this.state.password}
+            onChange={(e) => this.setState({password: e.target.value})} />
+        </label></p>
+        <p><button type="submit">Log In</button></p>
+      </form>
+    );
+  }
+}
 
 const LoginPage = () => (
   <div>
     <h1>Log In</h1>
-    <form onSubmit={(e) => {e.preventDefault();}}>
-      <p><label>username: <input type="text" name="username" /></label></p>
-      <p><label>password: <input type="password" name="password" /></label></p>
-      <p><button type="submit">Log In</button></p>
-    </form>
+    <Login />
   </div>
 );
 
