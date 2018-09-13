387cc163d3db578e4998ea43031c2722f9d8daa1
Henri Koskinen <hkoskinen@protonmail.com>
2018-09-13 20:59:02 +0300

    Fix nav links

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -6,12 +6,12 @@ const PageHeader = () => (
   <div style={{ marginBottom: '4em' }}>
     <Menu fixed='top' inverted size='large'>
       <Container>
-        <Menu.Item as='a' header><NavLink to='/'>remember</NavLink></Menu.Item>
+        <Menu.Item as={NavLink} to='/' exact header>remember</Menu.Item>
 
-        <Menu.Item as='a' position='right'><NavLink to='/login'>login</NavLink></Menu.Item>
-        <Menu.Item as='a'><NavLink to='/register'>register</NavLink></Menu.Item>
-        <Menu.Item as='a'><NavLink to='/dashboard'>dashboard</NavLink></Menu.Item>
-        <Menu.Item as='a'><NavLink to='/preferences'>preferences</NavLink></Menu.Item>
+        <Menu.Item as={NavLink} to='/login' position='right'>login</Menu.Item>
+        <Menu.Item as={NavLink} to='/register'>register</Menu.Item>
+        <Menu.Item as={NavLink} to='/dashboard'>dashboard</Menu.Item>
+        <Menu.Item as={NavLink} to='/preferences'>preferences</Menu.Item>
       </Container>
     </Menu>
   </div>
